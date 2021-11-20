0f589a31b73c0316ec241f00391ddd96bb797b8a
unsu0707 <unsu0707@gmail.com>
2021-11-20 22:36:56 +0900

    how to get multiple arguments

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	totalLength, _ := lenAndUpper("unsu0707")
-	fmt.Println(totalLength)
+	repeatMe("one", "two", "three", "four", "five")
 }
