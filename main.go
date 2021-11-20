f76a1d8dba2d5df85b7c01ad4e67ffc5aa1ebf3d
unsu0707 <unsu0707@gmail.com>
2021-11-20 23:05:15 +0900

    naked return

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func lenAndUpper(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
