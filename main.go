24d204ca55f61c3c1b1fb754ee7b84a75c9f8636
unsu0707 <unsu0707@gmail.com>
2021-11-21 12:55:43 +0900

    how to use switch with define var for itself

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 )
 
 func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
 		return false
+	case 18:
+		return true
 	}
-	return true
+	return false
 }
 
 func main() {
