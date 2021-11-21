41e9e679003530e7e24323de9ad199581316b816
unsu0707 <unsu0707@gmail.com>
2021-11-21 16:48:59 +0900

    add Add function of dictionary

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,6 +6,7 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("not found.")
+var errAlreadyExists = errors.New("already exists.")
 
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word]
@@ -13,4 +14,15 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	switch _, err := d.Search(word); err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errAlreadyExists
+	}
+	return nil
 }
